c7315e1bf03985b5b1cb501afd87e1fa34f3937d
kwasant7 <109564282+kwasant7@users.noreply.github.com>
2022-07-25 18:48:23 -0700

    general

---
+++ 0general/src/U20Dec_ABC.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class U20Dec_ABC {
+
+    static BufferedReader in;
+    static PrintWriter out;
+    static StringTokenizer st;    
+    static String f = "test";
+    static String n;
+    static int a,b,c,ab,bc,ca,abc;
+
+    public static void main(String[] args) throws IOException {
+        in = new BufferedReader(new FileReader(f+".in"));
+        out = new PrintWriter(new FileWriter(f+".out"));
+        init();
+        solve();
+        
+        in.close();
+        out.close();
+    }
+    
+    static void init() throws IOException {
+        n = in.readLine();
+        String[] p = n.split(" ");
+        int[] arr = new int[p.length];
+        for(int i =0;i<p.length;i++) {
+        	arr[i] = Integer.parseInt(p[i]);
+        }
+        Arrays.sort(arr);
+    }
+    
+    static void solve() {
+        String[] p = n.split(" ");
+        int[] arr = new int[p.length];
+        for(int i =0;i<p.length;i++) {
+        	arr[i] = Integer.parseInt(p[i]);
+        }
+        Arrays.sort(arr);
+    	a = arr[0];
+    	b = arr[1];
+    	abc = arr[6];
+    	c = abc-a-b;
+    	System.out.println(a+" "+b+" "+c);
+    	
+    
+    }
+
+}
